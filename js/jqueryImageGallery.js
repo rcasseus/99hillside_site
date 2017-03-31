25e3aee7c7b32bb1a90ef951c4478876e56a8dd8
rcasseus <rashan.casseus@gmail.com>
2017-03-30 20:33:59 -0400

    Hide last images and disable buttons
    Set up the floor plan page to hide the last two images unload of the
    page.
    Setting the buttons to be disabled when one is active. Bitches!!!!!

--- js/jqueryImageGallery.js
+++ js/jqueryImageGallery.js
@@ -1,9 +1,39 @@
 // Jquery Image Gallery
 // Code taken from http://codepen.io/solidx00/pen/wgGvyo
+// This code was first written for the floor plan page
 
 $( document ).ready(function() {
 
+  $("#img2, #img3").hide(); // on load hide the last two images
+
+  // Set buttons to disable one at a time.
+    $("#btn1").click(function() {
+        if ($("#btn2, #btn3").attr("disabled","disabled")) {
+            $("#btn2, #btn3").removeAttr("disabled","disabled");
+            $("#btn1").attr("disabled","disabled");
+          }
+      });
+
+
+    $("#btn2").click(function() {
+        if ($("#btn1, #btn2").attr("disabled","disabled")) {
+            $("#btn1, #btn2").removeAttr("disabled","disabled");
+            $("#btn2").attr("disabled","disabled");
+          }
+      });
+
+
+    $("#btn3").click(function() {
+        if ($("#btn2, #btn3").attr("disabled","disabled")) {
+            $("#btn2, #btn3").removeAttr("disabled","disabled");
+            $("#btn23").attr("disabled","disabled");
+        }
+    });
+
+
+
   $("#btn1").click(function(){
+    // $("#btn1").attr("disabled","disabled")
     $("#img1").fadeIn(400);
     $("#img2, #img3").hide();
     console.log ("Button 1 hit.");
@@ -11,6 +41,7 @@ $( document ).ready(function() {
   });
 
   $("#btn2").click(function(){
+    // $("#btn2").attr("disabled","disabled")
     $("#img2").fadeIn(400);
     $("#img1, #img3").hide();
     console.log ("Button 2 hit.");
@@ -18,6 +49,7 @@ $( document ).ready(function() {
   });
 
   $("#btn3").click(function(){
+    // $("#btn3").attr("disabled","disabled")
     $("#img3").fadeIn(400);
     $("#img1, #img2").hide();
     console.log ("Button 3 hit.");
